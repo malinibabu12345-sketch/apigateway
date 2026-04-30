0e3d6e3c38ca5f99a1ad9ba580b5d80074d9f9bc
Malini <malinibabu12345@gmail.com>
2026-04-30 17:43:40 +0530

    final fixes before deployment

--- src/main/java/com/guvi/apigateway/config/SecurityConfig.java
+++ src/main/java/com/guvi/apigateway/config/SecurityConfig.java
@@ -35,7 +35,10 @@ public class SecurityConfig {
                                 "/api/auth/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/v3/api-docs/**"
+                                "/v3/api-docs/**",
+                                "/v3/api-docs",
+                                "/swagger-resources/**",
+                                "/webjars/**"
                         ).permitAll()
                         .anyRequest().authenticated())
 
